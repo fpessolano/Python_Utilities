980c16d356a3779b7b697b9716a6bd30c0022779
Francesco Pessolano <fpessolano@users.noreply.github.com>
2022-01-20 11:34:20 +0000

    locks

--- locks.py
+++ locks.py
@@ -3,9 +3,14 @@ locks.py: support methods useable for locks and alike
 """
 
 from pathlib import Path
+from datetime import datetime
 
 
 def detect_new_day(lock_file: Path = Path('.daylock')):
+    """
+    Return true when executed on a different day than the last time.
+    It uses a file (lock_file) to keep track of the last day 
+    """
     day_of_the_year = datetime.now().timetuple().tm_yday
     new_day = True
     if lock_file.exists():
--- main.py
+++ main.py
@@ -2,13 +2,14 @@
 main.py: for testing and try outs
 """
 
-from locks import *
+import locks as lock
+
 from time import sleep
 from datetime import datetime
 
 if __name__ == "__main__":
     for _ in range(3):
-        if detect_new_day():
+        if lock.detect_new_day():
             print('Execute daily routine')
         print('Execute hourly routine')
         sleep((60 - datetime.now().timetuple().tm_min) * 60)
